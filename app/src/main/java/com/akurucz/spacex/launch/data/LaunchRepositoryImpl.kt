dee9b8ec6b2ca57c1a88829076c4b0c89ffca5d4
Andras Kurucz <an.kurucz@gmail.com>
2021-08-02 08:48:55 +0200

    organize imports

--- app/src/main/java/com/akurucz/spacex/launch/data/LaunchRepositoryImpl.kt
+++ app/src/main/java/com/akurucz/spacex/launch/data/LaunchRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.akurucz.spacex.launch.data
 
 import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.akurucz.spacex.launch.model.Launch
 import com.akurucz.spacex.launch.model.LaunchRepository
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class LaunchRepositoryImpl @Inject constructor(
