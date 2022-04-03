6fa7b37061debcff7961f8de065bc4b4ff42cd6e
Andras Kurucz <an.kurucz@gmail.com>
2022-04-03 11:11:50 +0200

    compose baseline

--- app/src/main/java/com/akurucz/spacex/MainActivity.kt
+++ app/src/main/java/com/akurucz/spacex/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.akurucz.spacex
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.compose.material.Text
+import com.akurucz.spacex.launch.ui.SpaceXTheme
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
-        setContentView(R.layout.main_activity)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appbarConfiguration = AppBarConfiguration(navController.graph)
-        setSupportActionBar(toolbar)
-        toolbar.setupWithNavController(navController, appbarConfiguration)
+        setContent {
+            SpaceXTheme() {
+                Text(text = "Hello Compose")
+            }
+        }
     }
 }
